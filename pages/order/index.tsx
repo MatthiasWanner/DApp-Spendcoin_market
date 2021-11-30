f8743b5470ed80052f90a10ee1b7b1179eca5cde
MatthiasWanner <matthias.wanner@gmail.com>
2021-11-30 15:42:35 +0100

    :white_check_mark: Update OrderForm , receive datas into onSubmit function

--- pages/order/index.tsx
+++ pages/order/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import OrderForm from '@components/OrderForm/OrderForm';
 
 export default function index() {
--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -1,10 +1,19 @@
 import { Button, TextInput, SelectInput } from '@components/FormComponents';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
 import OrderFormHeader from './OrderFormHeader';
 
 function OrderForm(): JSX.Element {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log('submit', data);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="shadow sm:rounded-md sm:overflow-hidden">
           <div className="bg-white py-6 px-4 space-y-6 sm:p-6">
             <OrderFormHeader />
@@ -16,6 +25,7 @@ function OrderForm(): JSX.Element {
                 className="col-span-6 sm:col-span-3"
                 inputClassName="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 labelClassName="block text-sm font-medium text-gray-700"
+                register={register}
               />
 
               <TextInput
@@ -24,6 +34,7 @@ function OrderForm(): JSX.Element {
                 className="col-span-6 sm:col-span-3"
                 inputClassName="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 labelClassName="block text-sm font-medium text-gray-700"
+                register={register}
               />
 
               <TextInput
@@ -32,31 +43,35 @@ function OrderForm(): JSX.Element {
                 className="col-span-6 sm:col-span-4"
                 inputClassName="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 labelClassName="block text-sm font-medium text-gray-700"
+                register={register}
               />
 
               <SelectInput
                 label="Pays"
-                value="country"
+                value="country-name"
                 options={[{ label: 'France', value: 'fr' }]}
                 className="col-span-6 sm:col-span-3"
                 labelClassName="block text-sm font-medium text-gray-700"
                 selectClassName="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                register={register}
               />
 
               <TextInput
                 label="Adresse postale"
-                value="streetAddress"
+                value="street-address"
                 className="col-span-6"
                 inputClassName="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 labelClassName="block text-sm font-medium text-gray-700"
+                register={register}
               />
 
               <TextInput
                 label="Ville"
-                value="city"
+                value="locality"
                 className="col-span-6 sm:col-span-6 lg:col-span-2"
                 inputClassName="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 labelClassName="block text-sm font-medium text-gray-700"
+                register={register}
               />
 
               <TextInput
@@ -65,11 +80,15 @@ function OrderForm(): JSX.Element {
                 className="col-span-6 sm:col-span-3 lg:col-span-2"
                 inputClassName="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 labelClassName="block text-sm font-medium text-gray-700"
+                register={register}
               />
             </div>
           </div>
           <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <Button className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <Button
+              type="submit"
+              className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
               {'Enregister ma commande'}
             </Button>
           </div>
