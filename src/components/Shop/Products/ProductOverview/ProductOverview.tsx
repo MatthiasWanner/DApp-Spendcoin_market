48424e3aff918f3c6658789a477f23236c7a7cb3
MatthiasWanner <matthias.wanner@gmail.com>
2021-12-01 11:52:39 +0100

    :construction: Add product overview component

---
+++ src/components/Shop/Products/ProductOverview/ProductOverview.tsx
@@ -0,0 +1,100 @@
+import { CheckIcon } from '@heroicons/react/solid';
+import ProductImages from './ProductImages';
+import { Button } from '@components/FormComponents';
+
+interface IProps {
+  product: any;
+}
+
+export default function ProductOverview({ product }: IProps): JSX.Element {
+  return (
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
+        {/* Product details */}
+        <div className="lg:max-w-lg lg:self-end">
+          <nav aria-label="Breadcrumb">
+            <ol role="list" className="flex items-center space-x-2">
+              {product.breadcrumbs.map(
+                (
+                  breadcrumb: { id: number; name: string; href: string },
+                  breadcrumbIdx: number
+                ) => (
+                  <li key={breadcrumb.id}>
+                    <div className="flex items-center text-sm">
+                      <a
+                        href={breadcrumb.href}
+                        className="font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        {breadcrumb.name}
+                      </a>
+                      {breadcrumbIdx !== product.breadcrumbs.length - 1 ? (
+                        <svg
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          aria-hidden="true"
+                          className="ml-2 flex-shrink-0 h-5 w-5 text-gray-300"
+                        >
+                          <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                        </svg>
+                      ) : null}
+                    </div>
+                  </li>
+                )
+              )}
+            </ol>
+          </nav>
+
+          <div className="mt-4">
+            <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              {product.name}
+            </h1>
+          </div>
+
+          <section aria-labelledby="information-heading" className="mt-4">
+            <h2 id="information-heading" className="sr-only">
+              Product information
+            </h2>
+
+            <div className="flex items-center">
+              <p className="text-lg text-gray-900 sm:text-xl">
+                {+product.price + (+product.VAT * +product.price) / 100}
+                {product.currencySymbol}
+                {' TTC'}
+              </p>
+            </div>
+
+            <div className="mt-4 space-y-6">
+              <p className="text-base text-gray-500">{product.description}</p>
+            </div>
+
+            <div className="mt-6 flex items-center">
+              <CheckIcon
+                className="flex-shrink-0 w-5 h-5 text-green-500"
+                aria-hidden="true"
+              />
+              <p className="ml-2 text-sm text-gray-500">
+                En stock. Prêt pour expédition
+              </p>
+            </div>
+          </section>
+        </div>
+
+        <ProductImages images={product.images} />
+
+        {/* Product form */}
+        <div className="mt-10 lg:max-w-lg lg:col-start-1 lg:row-start-2 lg:self-start">
+          <section aria-labelledby="options-heading">
+            <form>
+              <div className="mt-10">
+                <Button className="w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500">
+                  Acheter
+                </Button>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
