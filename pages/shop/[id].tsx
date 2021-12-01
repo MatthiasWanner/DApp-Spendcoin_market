fc5dfe5625cff544d345b9d3c31b43a0194a5823
MatthiasWanner <matthias.wanner@gmail.com>
2021-12-01 11:53:18 +0100

    :sparkles: display product details when clicking button

--- pages/shop/[id].tsx
+++ pages/shop/[id].tsx
@@ -1,15 +1,20 @@
+import { Overlay } from '@components/Modal';
+import { ProductOverview } from '@components/Shop';
 import { useRouter } from 'next/dist/client/router';
-import Link from 'next/link';
+import { gifCards } from '@datas/giftCards';
 
 export default function CardDetail() {
   const { id } = useRouter().query;
+  const router = useRouter();
+
   return (
     <>
       <div>Card detail: {id}</div>
       <div>
-        <Link href="/order">
-          <a>Acheter</a>
-        </Link>
+        <Overlay
+          handleClick={() => router.push('/shop')}
+          reactElement={<ProductOverview product={gifCards[+id! - 1]} />}
+        />
       </div>
     </>
   );
