1089c01793ce9bf1e47185a0565e694edbb52b24
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-12-02 13:34:22 +0100

    :pencil: write productOverview as redux store property of AppState

--- src/redux/slices/app.slice.ts
+++ src/redux/slices/app.slice.ts
@@ -1,13 +1,26 @@
+import { Product } from '@interfaces/Product';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../index';
 
 export interface AppState {
   activeModal: boolean;
+  productOverview: Product;
 }
 
 const initialState: AppState = {
-  activeModal: false
+  activeModal: false,
+  productOverview: {
+    name: '',
+    href: '',
+    basePrice: '',
+    currencySymbol: '',
+    VAT: '',
+    description: '',
+    images: [],
+    breadcrumbs: [],
+    options: []
+  }
 };
 
 export const appSlice = createSlice({
@@ -16,18 +29,23 @@ export const appSlice = createSlice({
   reducers: {
     setActiveModal: (state, action: PayloadAction<boolean>) => {
       return { ...state, activeModal: action.payload };
+    },
+    setproductOverview: (state, action: PayloadAction<Product>) => {
+      return { ...state, productOverview: { ...action.payload } };
     }
   }
 });
 
-export const { setActiveModal } = appSlice.actions;
+export const { setActiveModal, setproductOverview } = appSlice.actions;
 
 export const useAppFromStore = () => {
   const app = useSelector((state: RootState) => state.app);
   const dispatch = useDispatch();
   const dispatchActiveModal = (payload: boolean) =>
     dispatch(setActiveModal(payload));
-  return { app, dispatchActiveModal };
+  const dispatchproductOverview = (payload: Product) =>
+    dispatch(setproductOverview(payload));
+  return { app, dispatchActiveModal, dispatchproductOverview };
 };
 
 export default appSlice.reducer;
