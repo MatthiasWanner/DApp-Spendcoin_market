25692d857dcd27a83882a3e2b408783aa99b3c59
MatthiasWanner <matthias.wanner@gmail.com>
2021-12-03 08:09:59 +0100

    :iphone: fix backgound phone view

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -14,7 +14,7 @@ export default function Layout({ children }: IProps): JSX.Element {
   return (
     <div className="w-screen bg-space-cadet">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-        <div className="h-screen">
+        <div className="min-h-screen">
           <Header />
           <Body> {children} </Body>
         </div>
