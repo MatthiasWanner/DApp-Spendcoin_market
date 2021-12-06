cb75f2e482318c99768662b41aed8f6994b9d705
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-12-06 13:32:22 +0100

    :zap: change overview form button label

--- src/components/Shop/Products/ProductOverview/ProductOverViewForm.tsx
+++ src/components/Shop/Products/ProductOverview/ProductOverViewForm.tsx
@@ -65,7 +65,7 @@ function ProductOverViewForm({
               className="w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
               handleClick={() => handleBuyProduct(product, price)}
             >
-              Acheter
+              {'Ajouter au panier'}
             </Button>
           </div>
         </form>
