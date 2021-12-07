62c5de193ee5359b3d2ab8de38cadf998a4fda90
MatthiasWanner <matthias.wanner@gmail.com>
2021-12-07 19:32:56 +0100

    :construction: Add container to test loader

--- src/components/Payment/AwaitingRequest.tsx
+++ src/components/Payment/AwaitingRequest.tsx
@@ -2,10 +2,9 @@ function AwaitingRequest() {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col max-w-3xl mx-auto bg-red-600 h-72 shadow-white-light">
-        <svg
-          className="animate-spin h-5 w-5 mr-3 ..."
-          viewBox="0 0 24 24"
-        ></svg>
+        <div>
+          <div className="border-top-color:transparent w-16 h-16 border-4 border-blue-400 border-solid rounded-full animate-spin"></div>
+        </div>
         Processing
       </div>
     </div>
