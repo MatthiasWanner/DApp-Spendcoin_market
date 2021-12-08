c1768217f4cd2eda02b3c556e287819d7e643d89
MatthiasWanner <matthias.wanner@gmail.com>
2021-12-08 09:59:31 +0100

    :pencil2: Rename Spinner component folder

--- src/components/Payment/AwaitingRequest.tsx
+++ src/components/Payment/AwaitingRequest.tsx
@@ -1,4 +1,4 @@
-import Spinner from '@components/Spinner';
+import Spinner from '@components/SpinnerComponent';
 import { COLORS } from '@utils/constants/colors.constants';
 import PaymentContainer from './PaymentContainer';
 
