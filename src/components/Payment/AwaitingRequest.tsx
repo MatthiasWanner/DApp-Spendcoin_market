3586cb3421f63253a5014801b296d68a5166eff5
MatthiasWanner <matthias.wanner@gmail.com>
2021-12-08 08:41:14 +0100

    :white_check_mark: Create Spinner component. Apply it when request is loading

--- src/components/Payment/AwaitingRequest.tsx
+++ src/components/Payment/AwaitingRequest.tsx
@@ -1,11 +1,14 @@
+import Spinner from '@components/Spinner';
+import { COLORS } from '@utils/constants/colors.constants';
+
 function AwaitingRequest() {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="flex flex-col max-w-3xl mx-auto bg-red-600 h-72 shadow-white-light">
-        <div>
-          <div className="border-top-color:transparent w-16 h-16 border-4 border-blue-400 border-solid rounded-full animate-spin"></div>
-        </div>
-        Processing
+      <div className="flex flex-col max-w-3xl mx-auto bg-isabelline text-black h-72 shadow-white-light">
+        <Spinner
+          text="Votre facture est en cours de contôle, cela peut prendre plusieurs minutes"
+          spinnerColor={COLORS.INDIGO_DYE}
+        />
       </div>
     </div>
   );
---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,58 @@
+import { COLORS } from '@utils/constants/colors.constants';
+import React from 'react';
+
+interface IProps {
+  fullScreen?: boolean;
+  transparent?: boolean;
+  fixed?: boolean;
+  text?: string;
+  spinnerColor?: COLORS;
+}
+
+export default function Spinner({
+  fullScreen,
+  transparent,
+  fixed,
+  text,
+  spinnerColor = COLORS.WHITE
+}: IProps): JSX.Element {
+  return (
+    //OPTION CIRCLE
+    <div
+      className={`
+      ${
+        fullScreen
+          ? 'h-screen w-screen backdrop-filter backdrop-blur-sm'
+          : ' h-full w-full'
+      }
+      ${transparent ? 'bg-opacity-70' : ''}
+      ${fixed ? 'fixed top-0 left-0 z-50' : ''}
+      grid place-items-center
+    `}
+    >
+      <div className="flex flex-col items-center space-y-10">
+        <svg
+          className="animate-spin w-20 text-center"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke={spinnerColor}
+            strokeWidth="4"
+          />
+          <path
+            className="opacity-75"
+            fill={spinnerColor}
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
+        {text && <p className="text-black text-lg">{text}</p>}
+      </div>
+    </div>
+  );
+}
